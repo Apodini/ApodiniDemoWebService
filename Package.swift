2dcb037dad7b10698491831df2dfa406d3b7119a
Felix Desiderato <felix.desiderato@tum.de>
2021-06-17 09:40:15 +0200

    Override with local files

--- Package.swift
+++ Package.swift
@@ -1,22 +1,33 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-
 let package = Package(
-    name: "Apodini",
+    name: "TestWebService",
+    platforms: [.macOS(.v10_15)],
     products: [
-        .library(name: "Apodini", targets: ["Apodini"])
+        .executable(
+            name: "TestWebService",
+            targets: ["TestWebService"])
     ],
     dependencies: [
+        .package(url: "https://github.com/Apodini/Apodini.git", .branch("experimental/swift-arm"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "Apodini"),
-        .testTarget(name: "ApodiniTests",
-                    dependencies: [
-                        .target(name: "Apodini")
-                    ])
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TestWebService",
+            dependencies: [
+            .product(name: "Apodini", package: "Apodini"),
+            .product(name: "ApodiniREST", package: "Apodini"),
+            .product(name: "ApodiniOpenAPI", package: "Apodini")
+        ]),
+        .testTarget(
+            name: "TestWebServiceTests",
+            dependencies: ["TestWebService"]),
     ]
 )
